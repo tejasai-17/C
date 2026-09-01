86bbbe5001e7d6db65610a9f0f7a155d39f81e40
tejasai-17 <s.tejasai.17@gmail.com>
2026-09-01 22:03:31 +0530

    GCC

--- hello.c
+++ hello.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int a = 5;
-    int b = 5;
+    int a = 5, b = 5;
     printf("%d\n", a+b);
     return 0;
 }
---
+++ pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int a = 5;
+    int *p = &a;
+    printf("Value of a = %d\n", a);
+    printf("address of a = %p\n", &a);
+    printf("value of p = %d\n", *p);
+    printf("address of p = %p\n", p);
+    return 0;
+}
